1679487c7e999cf8fea95d162d0cfe46fe4866ab
Sachini Ginige <sachiniginige@gmail.com>
2023-05-27 08:16:31 +0530

    Update app-routing.module.ts

--- UserManagementApp/src/app/app-routing.module.ts
+++ UserManagementApp/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
+  {path:'', component:UsersListComponent},
   {path:'register', component:UserRegisterComponent}
 ];
 
