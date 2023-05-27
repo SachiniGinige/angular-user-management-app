007cb50262820d8905aa67cad02eec0b34f8432a
Sachini Ginige <sachiniginige@gmail.com>
2023-05-27 12:10:33 +0530

    Unit Tests created

--- UserManagementApp/src/app/services/user.service.spec.ts
+++ UserManagementApp/src/app/services/user.service.spec.ts
@@ -1,16 +1,75 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+// describe('UserService', () => {
+//   let service: UserService;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(UserService);
+//   });
+
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
+
+    userService = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
   });
+
+  it('should register a new user', () => {
+    const mockUser = {
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123'
+    };
+
+    userService.Register(mockUser).subscribe(response => {
+      expect(response).toEqual({ success: true });
+    });
+
+    const req = httpTestingController.expectOne('/api/users/register');
+    expect(req.request.method).toEqual('POST');
+    req.flush({ success: true });
+  });
+
+  it('should handle registration failure', () => {
+    const mockUser = {
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123'
+    };
+
+    userService.Register(mockUser).subscribe(
+      () => {},
+      error => {
+        expect(error).toEqual('Registration failed');
+      }
+    );
+
+    const req = httpTestingController.expectOne('/api/users/register');
+    expect(req.request.method).toEqual('POST');
+    req.error(new ErrorEvent('Registration failed'));
+  });
+
 });
--- UserManagementApp/src/app/user-register/user-register.component.spec.ts
+++ UserManagementApp/src/app/user-register/user-register.component.spec.ts
@@ -1,21 +1,101 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserRegisterComponent } from './user-register.component';
+import { UserService } from '../services/user.service';
+import { HttpClient } from '@angular/common/http';
+
+// describe('UserRegisterComponent', () => {
+//   let component: UserRegisterComponent;
+//   let fixture: ComponentFixture<UserRegisterComponent>;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [UserRegisterComponent]
+//     });
+//     fixture = TestBed.createComponent(UserRegisterComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
 
 describe('UserRegisterComponent', () => {
   let component: UserRegisterComponent;
   let fixture: ComponentFixture<UserRegisterComponent>;
+  let userService: UserService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserRegisterComponent],
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserRegisterComponent]
-    });
     fixture = TestBed.createComponent(UserRegisterComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should register a new user', fakeAsync(() => {
+    const mockUser = {
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123'
+    };
+
+    // Trigger registration
+    component.RegisterUser();
+
+    // Simulate HTTP request
+    const req = httpTestingController.expectOne('/api/users/register');
+    expect(req.request.method).toEqual('POST');
+    req.flush({ success: true });
+
+    // Advance to the response
+    tick();
+
+    // Assert the success message or any other behavior
+    // expect(component.registrationSuccess).toBeTrue();
+    // // ... additional assertions
+  }));
+
+  it('should handle registration failure', fakeAsync(() => {
+    const mockUser = {
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password123'
+    };
+
+    // Trigger registration
+    component.RegisterUser();
+
+    // Simulate HTTP request
+    const req = httpTestingController.expectOne('/api/users/register');
+    expect(req.request.method).toEqual('POST');
+    req.error(new ErrorEvent('registration failed'));
+
+    // Advance to the response
+    tick();
+
+    // Assert the failure message or any other behavior
+    // expect(component.registrationError).toBeTrue();
+    // // ... additional assertions
+  }));
+
 });
