2466ee2c2e3553616ebea4070d1599fad09d21c6
Sachini Ginige <sachiniginige@gmail.com>
2023-05-27 05:38:20 +0530

    Update project files

--- UserManagementApp/src/material.module.ts
+++ UserManagementApp/src/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -12,8 +14,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   exports: [
+    MatToolbarModule,
     MatButtonModule,
     MatInputModule,
+    MatFormFieldModule,
     MatSelectModule,
     MatCardModule,
     MatCheckboxModule,
