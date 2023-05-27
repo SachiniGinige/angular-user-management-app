c9fbad411bdee103886d116dbe6898068c4f6478
Sachini Ginige <sachiniginige@gmail.com>
2023-05-27 10:46:14 +0530

    Update material.module.ts

--- UserManagementApp/src/material.module.ts
+++ UserManagementApp/src/material.module.ts
@@ -3,14 +3,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+// import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatRadioModule } from '@angular/material/radio';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
@@ -18,14 +19,15 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    MatSelectModule,
+    // MatSelectModule,
     MatCardModule,
-    MatCheckboxModule,
-    MatRadioModule,
+    // MatCheckboxModule,
+    // MatRadioModule,
     MatPaginatorModule,
     MatTableModule,
     MatSortModule,
     MatDialogModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
